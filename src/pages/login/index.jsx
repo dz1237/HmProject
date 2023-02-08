b2ee6e1cbebacd1c1055526bbfe5f939c058287d
dz1237 <2366987785@qq.com>
2023-02-08 12:45:04 +0800

    更新antd

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import {Button} from 'antd'
 export default function Login() {
   return (
     <div>
     xixixi1
+    <Button type='primary'>我是青年牛</Button>
     </div>
   );
 }
