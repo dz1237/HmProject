d133a11754964fef321e785c438b4ca5dc0af62b
dz1237 <2366987785@qq.com>
2023-02-08 12:39:03 +0800

    更新路由

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
+import { BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom'
 import './App.css';
-
+import Login from './pages/login/index.jsx'
+import Home from './pages/layout/index.jsx'
 function App() {
   return (
-    <div className="App">
-     
+    <Router>
+    <div className="App"> 
+      <Link to='/login'>登录</Link>
+      <Link to='/home'>首页</Link>
+      <Routes>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/home' element={<Home/>}></Route>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
